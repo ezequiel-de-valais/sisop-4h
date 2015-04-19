5a839719c3b863698ada2cf5b12472fac36b72bf
ezequiel-de-valais <ezequiel.devalais@gmail.com>
2015-04-19 18:17:43 -0300

    gitignore y modificacion a glog

--- test/loggerTest.sh
+++ test/loggerTest.sh
@@ -4,7 +4,7 @@
 #echo $0
 #dirname $0
 #basename $0
-#bash Glog.sh auxiliarCommand mensaje1 debug
+#bash glog auxiliarCommand mensaje1 debug
 #./Glog.sh auxiliarCommand mensaje2 debug
 #./Glog.sh auxiliarCommand mensaje3 debug
 
@@ -15,7 +15,7 @@ cd "$_DIR"
 #export Glog=${_DIR}bin/Glog.sh
 #log="./Glog.sh"
 
-./bin/Glog.sh auxiliarCommand mensaje1 debug
-./bin/Glog.sh anOtherCommand mensaje2 error
-./bin/Glog.sh auxiliarCommand mensaje3
-
+./bin/glog auxiliarCommand mensaje1 INFO
+./bin/glog anOtherCommand mensaje2 ERR
+./bin/glog auxiliarCommand mensaje3 WAR
+./bin/glog command "este es el mensaje que genera command" INFO
