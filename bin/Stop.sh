f902011992d54bdd5c23fa4e832063338a5950af
ezequiel-de-valais <ezequiel.devalais@gmail.com>
2015-04-21 22:29:09 -0300

    fix kill myself

--- bin/Stop.sh
+++ bin/Stop.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Objetivos:
 # . Detener procesos
-# Restricciones:
-# . No se puede arrancar un proceso que no existe WTF
-# . No se puede arrancar un proceso si no se inicializo el ambiente
 
 # Parametro1 : Nombre del proceso
 
@@ -12,17 +9,15 @@ Proceso=$1
 # para verificar que no esta corriendo
 # Obtengo procesos con ps ax , uso grep para quedarme con los que no son $$ (este proceso),
 # que no sean grep , que no sea Start.sh y que matchee cn el nombre del proceso
-ProcesosCorriendo=$(ps ax | grep -v $$ | grep -v "grep" | grep -v "Start.sh" | grep $Proceso)
+ProcesosCorriendo=$(ps ax | grep -v $$ | grep -v "grep" | grep -v "Stop.sh" | grep $Proceso)
 # Del filtro anterior , me quedo con la primer linea , y de la primer linea saco los primeros 4 bytes 
-PIDproceso=$(echo $ProcesosCorriendo | head -n 1 | head -c 4)
+PIDproceso=$(echo "$ProcesosCorriendo" | head -n 1 | head -c 4)
 if [ "$PIDproceso" == "" ]; then
 	echo "proceso no existe"
 	#Escribir en el log que el archivo se esta ejecutando
 else
-	echo "$PIDproceso"
 	# Detengo el proceso
 	kill "$PIDproceso"
 fi
 
  
-echo "" | head -n 1 | head -c 4
