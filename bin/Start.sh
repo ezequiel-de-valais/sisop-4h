431e4b4819cc51b2a45a7b85d7a7b852aa156d25
Juan <pc@ubuntu>
2015-04-21 22:32:54 -0300

    prueba

--- bin/Start.sh
+++ bin/Start.sh
@@ -15,7 +15,7 @@ Proceso=$1
 # que no sean grep , que no sea Start.sh y que matchee cn el nombre del proceso
 ProcesosCorriendo=$(ps ax | grep -v $$ | grep -v "grep" | grep -v "Start.sh" | grep $Proceso)
 # Del filtro anterior , me quedo con la primer linea , y de la primer linea saco los primeros 4 bytes 
-PIDproceso=$(echo $ProcesosCorriendo | head -n 1 | head -c 4)
+PIDproceso=$(echo $ProcesosCorriendo | head -n 1 | head -c 5)
 if [ "$PIDproceso" == "" ]; then
 	# Inicio el proceso
 	bash $Proceso &
--- bin/Stop.sh
+++ bin/Stop.sh
@@ -12,9 +12,9 @@ Proceso=$1
 # para verificar que no esta corriendo
 # Obtengo procesos con ps ax , uso grep para quedarme con los que no son $$ (este proceso),
 # que no sean grep , que no sea Start.sh y que matchee cn el nombre del proceso
-ProcesosCorriendo=$(ps ax | grep -v $$ | grep -v "grep" | grep -v "Start.sh" | grep $Proceso)
+ProcesosCorriendo=$(ps ax | grep -v $$ | grep -v "grep" | grep -v "Stop.sh" | grep $Proceso)
 # Del filtro anterior , me quedo con la primer linea , y de la primer linea saco los primeros 4 bytes 
-PIDproceso=$(echo $ProcesosCorriendo | head -n 1 | head -c 4)
+PIDproceso=$(echo $ProcesosCorriendo | head -n 1 | head -c 5)
 if [ "$PIDproceso" == "" ]; then
 	echo "proceso no existe"
 	#Escribir en el log que el archivo se esta ejecutando
