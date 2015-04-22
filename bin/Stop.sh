89ceb796bed88467b96348b119afcfe44e62b638
Juan <pc@ubuntu>
2015-04-21 23:14:12 -0300

    correccion

--- bin/Stop.sh
+++ bin/Stop.sh
@@ -11,7 +11,7 @@ Proceso=$1
 # que no sean grep , que no sea Start.sh y que matchee cn el nombre del proceso
 ProcesosCorriendo=$(ps ax | grep -v $$ | grep -v "grep" | grep -v "Stop.sh" | grep $Proceso)
 # Del filtro anterior , me quedo con la primer linea , y de la primer linea saco los primeros 4 bytes 
-PIDproceso=$(echo "$ProcesosCorriendo" | head -n 1 | head -c 4)
+PIDproceso=$(echo "$ProcesosCorriendo" | head -n 1 | head -c 5)
 if [ "$PIDproceso" == "" ]; then
 	echo "proceso no existe"
 	#Escribir en el log que el archivo se esta ejecutando
