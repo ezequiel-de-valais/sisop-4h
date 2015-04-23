a7201dff5a5970223bcb6138f4e5939a2f9c9494
Juan <pc@ubuntu>
2015-04-22 23:14:42 -0300

    RecPro inicial

---
+++ bin/RecPro.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Demonio 
+# Se dispara por Start
+# Se detiene con Stop
+# Graba en el log a traves de Glog
+# Mueve archivos con Mover
+# Puede invocar a ProPro
+# Duerme 
+
+# Tiempo que duerme
+intervalo=10
+
+ciclo=0
+
+while true
+do
+	let ciclo=ciclo+1
+	#Escribir el numero de ciclo en el LOG
+	#Validar los archivos en el directorio de novedades
+	#Si corresponde disparar ProPro
+	sleep $intervalo
+done
