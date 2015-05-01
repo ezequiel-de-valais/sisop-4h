ff66b48dcbb94feaffc117574f5b5ba13cb10077
Juan <pc@ubuntu>
2015-05-01 16:02:42 -0300

    agsa

--- bin/RecPro.sh
+++ bin/RecPro.sh
@@ -9,11 +9,11 @@
 
 # Tiempo que duerme
 intervalo=10
-#GRUPO=/home/pc/Escritorio/TPSO/Git/sisop-4h
-#MAEDIR=20115-1C-Datos/maestros
-#NOVEDIR=20115-1C-Datos/
-#ACEPDIR=bin/aceptados
-#RECHDIR=bin/rechazados
+GRUPO=/home/pc/Escritorio/TPSO/Git/sisop-4h
+MAEDIR=maestros
+NOVEDIR=20115-1C-Datos/
+ACEPDIR=bin/aceptados
+RECHDIR=bin/rechazados
 NOVEDADES="$GRUPO/$NOVEDIR/"
 EMISORES="$GRUPO/$MAEDIR/emisores.mae"
 NORMAS="$GRUPO/$MAEDIR/normas.mae"
@@ -32,6 +32,7 @@ function validar_formato_nombre (){
 	archivos_a_rechazados=$(ls -1 $NOVEDADES | grep -v "^.*_.*_.*_.*_.*$") 
 	for archivo in $archivos_a_rechazados;do
 		./Mover.sh "$NOVEDADES/$archivo" "$RECHAZADOS"
+		./Glog.sh "RecPro.sh" "Rechazado por formato invalido" INFO
 		#escribir log
 	done
 }
@@ -55,20 +56,23 @@ function validar_fecha (){
 	#Corto en dia,mes y año cada una de las fechas
 	di=$(echo $fechaInicial | cut -d "/" -f 1)
 	df=$(echo $fechaFinal | cut -d "/" -f 1)
-	d3=$(echo $fechaValidar | cut -d "/" -f 1)
+	d3=$(echo $fechaValidar | cut -d "-" -f 1)
 	mi=$(echo $fechaInicial | cut -d "/" -f 2)
 	mf=$(echo $fechaFinal | cut -d "/" -f 2)
-	m3=$(echo $fechaValidar | cut -d "/" -f 2)
+	m3=$(echo $fechaValidar | cut -d "-" -f 2)
 	ai=$(echo $fechaInicial | cut -d "/" -f 3)
 	af=$(echo $fechaFinal | cut -d "/" -f 3)
-	a3=$(echo $fechaValidar | cut -d "/" -f 3)
+	a3=$(echo $fechaValidar | cut -d "-" -f 3)
 	#Invierto el orden de las fechas para poder compararlas (año/mes/dia)
 	fechaInicial="$ai$mi$di"
 	fechaFinal="$af$mf$df"
 	fechaValidar="$a3$m3$d3"
-	if [[ "$fechaInicial" > "$fechaValidar" ]];then
+	if [[ "$fechaInicial" > "$fechaValidar" ]]; then
+		#Antes de la fecha inicial
 		return 1
+	fi
 	if [[ "$fechaFinal" < "$fechaValidar" ]]; then
+		#Despues de la fecha final
 		return 1
 	fi	
 	return 0
@@ -81,7 +85,7 @@ ciclo=0
 while true
 do
 	let ciclo=ciclo+1
-	./Glog "RecPro.sh" "ciclo numero : $ciclo" INFO
+	./Glog.sh "RecPro.sh" "ciclo numero : $ciclo" INFO
 	#Escribir el numero de ciclo en el LOG
 	#Validar los archivos en el directorio de novedades
 	hay_archivos
@@ -98,17 +102,26 @@ do
 			fecha=$(echo $archivo | cut -d "_" -f 5)
 			if [ $(grep -c "^$gestion;.*;.*;.*;.*$" "$GESTIONES") -eq 0 ];then
 				./Mover.sh "$NOVEDADES/$archivo" "RECHAZADOS"
+				./Glog.sh "RecPro.sh" "$archivo rechazados gestion invalida" INFO
 				#Loggear	
 			elif [ $(grep -c "^$norma;.*;.*$" "$NORMAS") -eq 0 ];then
 				./Mover.sh "$NOVEDADES/$archivo" "RECHAZADOS"
+				./Glog.sh "RecPro.sh" "$archivo rechazados cod_norma invalido" INFO
 				#Loggear
 			elif [ $(grep -c "^$emisor;.*;.*;.*$" "$EMISORES") -eq 0 ];then
 				./Mover.sh "$NOVEDADES/$archivo" "RECHAZADOS"
+				./Glog.sh "RecPro.sh" "$archivo rechazados cod_emisor invalido" INFO
 				#Loguear
 			else 
-				mkdir -p "ACEPTADOS/$gestion"
-				./Mover.sh "$NOVEDADES/$archivo" "ACEPTADOS/$gestion"
-				#Loggear
+				validar_fecha
+				if [ $? = 1 ];then
+					./Mover.sh "$NOVEDADES/$archivo" "$RECHAZADOS"
+					./Glog.sh "RecPro.sh" "$archivo rechazado fecha invalida" INFO
+				else
+					mkdir -p "$ACEPTADOS/$gestion"
+					./Mover.sh "$NOVEDADES/$archivo" "$ACEPTADOS/$gestion"
+					./Glog.sh "RecPro.sh" "$archivo aceptados" INFO
+				fi
 			fi
 		done
 
