1a77a87c5591caeb444cb5ff0d6dad9aca2ce7e9
ezequiel-de-valais <ezequiel.devalais@gmail.com>
2015-05-03 20:12:29 -0300

    add Pro (incompleto) y RecPro

--- bin/ProPro.sh
+++ bin/ProPro.sh
@@ -17,7 +17,6 @@
 # $2 = tipo (INF WAR ERR)
 
 function grabarLog {
-
    ./Glog.sh "ProPro" "$1" "$2"
 
 }
@@ -26,22 +25,20 @@ function grabarLog {
 
 function validarEjecucionIniPro {
 
-  variables=(GRUPO BINDIR MAEDIR NOVEDIR ACEPDIR RECHDIR PROCDIR INFODIR DUPDIR LOGDIR LOGSIZE)
+  variables=($GRUPO $BINDIR $MAEDIR $ACEPDIR $RECHDIR $PROCDIR)
   for var in ${variables[*]}; do
-      res=`env | grep $var | cut -d"=" -f 2`
-      if [ -z $res ]; then
-         return 1
+      if [ -z "$var" ]; then
+         return 0
       fi
-  done	
-  return 0
-
+  done  
+  return 1
 }
 
 # Verifica que el archivo aceptado no haya sido procesado anteriormente
 
 function verificarDuplicado {
-
-  if [ -f $2/$1 ]; then
+    grabarLog "$2/$1" INFO
+  if [ -f "$2/$1" ]; then
     return 0
   else
     return 1
@@ -52,96 +49,62 @@ function verificarDuplicado {
 # Verifica que la combinacion COD_NORMA/COD_EMISOR sea valida
 
 function verificarNormaEmisor {
-
-  if [ $(grep -c "$1;$2" "$GRUPO$MAEDIR/tab/nxe.tab") -eq 0 ]; then
+  nxefile="$GRUPO$MAEDIR/tab/nxe.tab"
+  #echo "NXEFILE $nxefile"
+  res=$(grep -c "$1;$2" "$nxefile")
+  #echo $res
+  if [ $res -eq 0 ]; then
     return 0
   else
     return 1
   fi
-
-}
-
-# Valida que el registro sea válido
-# $1=archivo a procesar
-# $2=gestion
-
-function validadorRegistro {
-
-   while read line || [[ -n "$line" ]]; do 
-	validarFechaRegistro "$2"
-   done < $1
-
-}
-
-# Valida que el registro sea válido
-# $1=gestion
-
-function validadorFechaRegistro {
-   
-   
-
-}
-
-# Genera el archivo de registros rechazados
-# $1=gestion
-# $2=registro
-
-function rechazarRegistro {
-
-   RECHFILE="$GRUPO${PROCDIR}/$1.rech"
-   echo "$2" >> "$RECHFILE"
-
 }
 
 # Funcion principal
 
 function main {
    
-   CONFDIR=../conf
-   confFile=InsPro.conf
    validarEjecucionIniPro
    validacion=$?
-
-   if [ $validacion -eq 1 ]; then 
+   if [ $validacion -eq 0 ]; then 
       grabarLog "El ambiente no está inicializado." "ERR"
       grabarLog "No se ejecutará el programa ProPro." "ERR"
    else
       grabarLog "Inicio de ProPro." "INF"
-      cantidadArchivos=`find $GRUPO$NOVEDIR$ACEPDIR -type f | wc -l`
+      cantidadArchivos=`find $GRUPO$ACEPDIR -type f | wc -l`
       grabarLog "Cantidad de archivos a procesar: $cantidadArchivos" "INF"
       MAESTROGESTIONES="$GRUPO$MAEDIR/gestiones.mae"
-      gestiones=""
       while read line || [[ -n "$line" ]]; do 
           gestiones+=$(echo $line | cut -d ";" -f1) 
           gestiones+=" "
       done < $MAESTROGESTIONES
-      echo -e ${gestiones[*]}
       for gestion in ${gestiones[*]}; do
-          if [ `ls $GRUPO$NOVEDIR$ACEPDIR | grep -c $gestion` != 0 ]; then
-             if [ `ls $GRUPO$NOVEDIR$ACEPDIR/$gestion | cut -d"_" -f1 | grep -c $gestion` != 0 ]; then #Hay al menos un arch de la gestion
-		fechasordenadas=$(ls $GRUPO$NOVEDIR$ACEPDIR/$gestion | cut -d"_" -f5 | sort -k1.7 -k1.4 -k1.1)
-		for fecha in $fechasordenadas; do
-		    for archivo in `ls $GRUPO$NOVEDIR$ACEPDIR/$gestion | grep $fecha`; do
-          	    	grabarLog "Archivo a procesar: $archivo" "INF"
-          	    	verificarDuplicado $archivo "$GRUPO$NOVEDIR$PROCDIR/proc"
-          	    	if [ $? == 0 ]; then   #Si esta duplicado
-             		    grabarLog "Se rechaza el archivo por estar DUPLICADO." "WAR"
-             		    Mover.sh "$GRUPO$NOVEDIR$ACEPDIR/$gestion/$archivo" "$GRUPO$NOVEDIR$RECHDIR" "ProPro"
-          	    	else
-	     		    norma=$(echo $archivo | cut -d "_" -f 2)
-			    emisor=$(echo $archivo | cut -d "_" -f 3)
-	                    verificarNormaEmisor $norma $emisor
-			    if [ $? == 0 ]; then   #La combinacion COD_NORMA/COD_EMISOR no se encuentra en la tabla nxe.tab
-             		       grabarLog "Se rechaza el archivo. Emisor no habilitado en este tipo de norma." "WAR"
-             		       Mover.sh "$GRUPO$NOVEDIR$ACEPDIR/$gestion/$archivo" "$GRUPO$NOVEDIR$RECHDIR" "ProPro"
-          	    	    else
-			       validadorRegistro "$GRUPO$NOVEDIR$ACEPDIR/$gestion/$archivo" "$gestion"
-          	    	    fi
-          	    	fi
-		    done
-      		done
-	     fi
-	  fi
+          if [ `ls $GRUPO$ACEPDIR | grep -c $gestion` != 0 ]; then
+             if [ `ls $GRUPO$ACEPDIR/$gestion | cut -d"_" -f1 | grep -c $gestion` != 0 ]; then #Hay al menos un arch de la gestion
+    fechasordenadas=$(ls $GRUPO$ACEPDIR/$gestion | cut -d"_" -f5 | sort -k1.7 -k1.4 -k1.1)
+    for fecha in $fechasordenadas; do
+        for archivo in `ls $GRUPO$ACEPDIR/$gestion | grep $fecha`; do
+                  grabarLog "Archivo a procesar: $archivo" "INF"
+                  verificarDuplicado "$archivo" "$GRUPO$PROCDIR/proc"
+                  if [ $? == 0 ]; then   #Si esta duplicado
+                    grabarLog "Se rechaza el archivo por estar DUPLICADO." "WAR"
+                    ./Mover.sh "$GRUPO$ACEPDIR/$gestion/$archivo" "$GRUPO$RECHDIR" "ProPro"
+                  else
+                      norma=$(echo $archivo | cut -d "_" -f 2)
+                      emisor=$(echo $archivo | cut -d "_" -f 3)
+                      verificarNormaEmisor $norma $emisor
+                      if [ $? == 0 ]; then   #La combinacion COD_NORMA/COD_EMISOR no se encuentra en la tabla nxe.tab
+                        grabarLog "Se rechaza el archivo. Emisor no habilitado en este tipo de norma." "WAR"
+                        Mover.sh "$GRUPO$ACEPDIR/$gestion/$archivo" "$GRUPO$RECHDIR" "ProPro"
+                      else
+                         #TODO Falta la validación por registro
+                         echo -e "Falta la validación por registro." 
+                      fi
+                  fi
+        done
+          done
+       fi
+    fi
       done
    fi 
 
